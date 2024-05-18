e65979318beaa2b867054c5235bd777f009f686e
Bertie Cartwright <sunstonesharptor@gmail.com>
2024-05-18 15:57:14 +0100

    Set up CMake and modified code to allow compilation with GCC

---
+++ build/CMakeFiles/sunstone.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/media/bertie-cartwright/External 4TB HDD/sunst/Documents/C++/Chess 3.0/Sunstone/bin/Linux/Debug/sunstone"
+  "/media/bertie-cartwright/External 4TB HDD/sunst/Documents/C++/Chess 3.0/Sunstone/bin/Linux/Debug/sunstone.pdb"
+  "CMakeFiles/sunstone.dir/src/board.cpp.o"
+  "CMakeFiles/sunstone.dir/src/board.cpp.o.d"
+  "CMakeFiles/sunstone.dir/src/engine.cpp.o"
+  "CMakeFiles/sunstone.dir/src/engine.cpp.o.d"
+  "CMakeFiles/sunstone.dir/src/main.cpp.o"
+  "CMakeFiles/sunstone.dir/src/main.cpp.o.d"
+  "CMakeFiles/sunstone.dir/src/transpositionTable.cpp.o"
+  "CMakeFiles/sunstone.dir/src/transpositionTable.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sunstone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sunstone.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sunstone.
+# This may be replaced when dependencies are built.
